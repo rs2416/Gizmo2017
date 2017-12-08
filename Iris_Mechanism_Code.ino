beaf929184fa6df6535f25ab4449996c3e91505e
Ruksana Shaukat <rs2416@ic.ac.uk>
2017-12-08 19:16:56 +0000

    Add files via upload

---
+++ Iris_Mechanism_Code.ino
@@ -0,0 +1,60 @@
+//---------SETUP---------------
+//Photoresistor connected to pin A0
+//Servo connected to pin 9
+
+
+
+//---IRIS LIGHT SENSOR
+
+const int light_pin = A0;  // pin with the photoresistor 
+int light_value = 0; // variable to hold the value from the sensor
+
+//----SERVO 
+
+#include <Servo.h>
+Servo myservo;  // create servo object to control a servo
+
+//----CONSTANTS
+const int ambient_light = 870; //setting ambient conditions/light threshold
+const int low_light = ambient_light - 75; //setting the low light condition and high light condition 
+const int high_light = ambient_light + 75;
+
+
+const int iris_neutral = 90;    // variable to store the servo position
+const int iris_open = 0; //servo position for open eye
+const int iris_closed = 150;  //servo position for closed eye
+
+void setup() {
+
+  //-------SERIAL AND SERVO
+
+  Serial.begin(9600);
+  myservo.attach(9);
+  myservo.write(iris_neutral);
+
+}
+
+void loop() {
+  check_light(); //check_light function
+}
+
+void check_light() {
+  light_value = analogRead(light_pin); //assigning the light value to the raw analog reading from the light sensor
+  Serial.println(light_value);
+  delay(500);
+
+  if (low_light <= light_value <= high_light) { //if light value is greater than or equal to "low light" and less than or equal to "high light", the servo moves to neutral position
+    myservo.write(iris_neutral);
+  }
+  
+  if (light_value >= high_light) { //if light value is greater or equal to "high light", servo moves to iris open position
+    myservo.write(iris_closed);
+  }
+
+  if (light_value <= low_light) { //if light value is less than or equal to low light, servo moves to iris open position
+    myservo.write(iris_open);
+  }
+
+  
+}
+
---
+++ Main_Eye_Movement.ino
@@ -0,0 +1,193 @@
+//-----SETUP------
+
+//Sharp IR sensor 1 connected to A0 pin
+//Sharp IR sensor 2 connected to A1 pin
+
+//Stepper Motor connected with dual motor driver
+
+
+const int IR_threshold = 10;
+const int stepper_speed = 10;
+
+int last_position = 0; // 0 = centre, -1 = left, 1 = right
+
+
+//---MAIN MOVEMENT WITH IR 
+
+#define sensorL A0 // Sharp IR Sensor 1 - LEFT
+#define sensorR A1 // Sharp IR Sensor 2 - RIGHT
+
+int left_distance;
+int right_distance;
+
+
+//----STEPPER
+
+#include <Stepper.h>
+
+const int stepsPerRevolution = 30;  // change this to fit the number of steps per revolution
+// for your motor
+
+int right = stepsPerRevolution; 
+int left = -stepsPerRevolution;
+int right_to_left = -2*stepsPerRevolution;
+int left_to_right = 2*stepsPerRevolution;
+
+// initialize the stepper library on pins 8 through 11:
+Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
+
+
+//------------------------------------------------------------SETUP------------------------------------------------------------
+void setup() {
+  
+
+//-------SERIAL
+
+  Serial.begin(9600);
+
+//------Stepper
+
+  myStepper.setSpeed(stepper_speed);
+}
+
+
+//-----------------------------------------------------------MAIN CODE------------------------------------------------------------------------
+void loop() {
+
+//----------SETUP FOR IR Sensors
+
+//IR (Left)
+  float voltsL = analogRead(sensorL)*0.0048828125;  // value from sensor * (5/1024)
+  left_distance = 13*pow(voltsL, -1);
+  delay(100);
+
+//IR (Right)
+  float voltsR = analogRead(sensorR)*0.0048828125;  // value from sensor * (5/1024)
+  right_distance = 13*pow(voltsR, -1);
+  delay(100);
+
+
+
+
+
+
+
+
+  if (last_position == 0) { //ie. If the eye is in the centre
+    if ((left_distance < IR_threshold) && (right_distance > IR_threshold)) { //Left IR sensed, turn left
+      Serial.println("Left IR SENSED");
+      Serial.println( "Left IR = ");Serial.println(left_distance);
+      Serial.println("Right IR = ");Serial.println(right_distance);
+      delay(100);
+
+      Serial.println("left from centre");                         
+      myStepper.step(left);
+      last_position = -1;
+      delay(100);
+      }
+
+    else if ((left_distance > IR_threshold) && (right_distance < IR_threshold)) { //Right IR sensed, turn right
+      Serial.println("Right IR SENSED");
+      Serial.println( "Left IR = ");Serial.println(left_distance);
+      Serial.println("Right IR = ");Serial.println(right_distance);
+      delay(100);
+
+      Serial.println("right from centre");                         
+      myStepper.step(right);
+      last_position = 1;
+      delay(100);
+      }
+
+//    else {
+//      last_position = 0;
+//    }
+  }
+  
+  
+  if (last_position == 1) { //ie. Eye is on the right
+    if((left_distance < IR_threshold) && (right_distance > IR_threshold)) { //Left IR sensed, go all the way left
+
+      Serial.println("Left IR SENSED");
+      Serial.println( "Left IR = ");Serial.println(left_distance);
+      Serial.println("Right IR = ");Serial.println(right_distance);
+      delay(100);
+
+      Serial.println("left from right");                         
+      myStepper.step(right_to_left);
+      last_position = -1;
+      delay(100);
+      }
+    
+    else if((left_distance > IR_threshold) && (right_distance > IR_threshold)) { //nothing sensed, return to centre
+      Serial.println("Nothing sensed");
+      Serial.println( "Left IR = ");Serial.println(left_distance);
+      Serial.println("Right IR = ");Serial.println(right_distance);
+      delay(100);
+
+      Serial.println("right to centre");                         
+      myStepper.step(left);
+      last_position = 0;
+      delay(100);
+      }
+    else if((left_distance < IR_threshold) && (right_distance < IR_threshold)) { //both sensed, go to centre
+      Serial.println("Both sensed");
+      Serial.println( "Left IR = ");Serial.println(left_distance);
+      Serial.println("Right IR = ");Serial.println(right_distance);
+      delay(100);
+    
+      Serial.println("right to centre");                         
+      myStepper.step(left);
+      last_position = 0;
+      delay(100);
+      }
+//    else {
+//      last_position = 1;
+//      }         
+  }
+
+  if (last_position == -1) {
+    if((left_distance > IR_threshold) && (right_distance < IR_threshold)) { //Right IR sensed, go all the way right
+      Serial.println("Right IR SENSED");
+      Serial.println( "Left IR = ");Serial.println(left_distance);
+      Serial.println("Right IR = ");Serial.println(right_distance);
+      delay(100);
+
+      Serial.println("right from left");                         
+      myStepper.step(left_to_right);
+      last_position = 1;
+      delay(100);
+      }
+
+    else if((left_distance > IR_threshold) && (right_distance > IR_threshold)) { //nothing sensed, return to centre
+      Serial.println("Nothing sensed");
+      Serial.println( "Left IR = ");Serial.println(left_distance);
+      Serial.println("Right IR = ");Serial.println(right_distance);
+      delay(100);
+
+      Serial.println("left to centre");                         
+      myStepper.step(right);
+      last_position = 0;
+      delay(100);
+      }
+
+    else if((left_distance < IR_threshold) && (right_distance < IR_threshold)) { //both sensed, go to centre
+      Serial.println("Both sensed");
+      Serial.println( "Left IR = ");Serial.println(left_distance);
+      Serial.println("Right IR = ");Serial.println(right_distance);
+      delay(100);
+    
+      Serial.println("left to centre");                         
+      myStepper.step(right);
+      last_position = 0;
+      delay(100);  
+      }
+
+//    else {
+//      last_position = -1;
+//      }
+  }
+}
+
+
+
+
